e32e415455e2f0607319d82af8eb958c5b490e9d
Ernesto Cisnero <miguelernestocisnero@gmail.com>
2026-01-27 11:35:54 +0100

    Initial Commit

---
+++ PlayConnect/PlayConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayConnectApp.swift
+//  PlayConnect
+//
+//  Created by Ernesto Cisnero on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct PlayConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
